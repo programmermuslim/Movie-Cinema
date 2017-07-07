48550377a7986f30c109338675aae2c226422442
Rizki@Imastudio <rizki@imastudio.co.id>
2017-07-07 20:37:48 +0700

    Cinema Movie
    Add Main Activity

---
+++ CinemaMovie/app/src/main/java/imastudio/rizki/com/cinemamovie/MainActivity.java
@@ -0,0 +1,13 @@
+package imastudio.rizki.com.cinemamovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
